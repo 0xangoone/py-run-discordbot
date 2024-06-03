8fd7a63064da2cce0cdd67b6109487635e2addba
عبد الرحمن <144622489+ango7581@users.noreply.github.com>
2024-06-03 17:26:00 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+const { Client, Events, GatewayIntentBits,Collection } = require("discord.js");
+const { exec } = require('child_process');
+const fs = require('fs');
+const { stderr } = require("process");
+const {token} = require("./token.json")
+
+
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageTyping,
+        GatewayIntentBits.MessageContent,
+    ] 
+});
+client.once(Events.ClientReady, c => {
+	console.log('Ready!');
+});
+client.on('ready', () =>{
+})
+client.on('messageCreate', message => {
+    console.log(`Received message: ${message.content}`);
+    
+    
+    if (message.content.includes("السلام عليكم") || message.content.includes("السلام عليكن ورحمة الله وبركاته")) {
+        
+        if (message.channel.id !== '1226176448612663376') {
+            message.reply(`وعليكم السلام ورحمة الله وبركاته ${message.author.username}`);
+        }
+        return;
+    }
+
+    
+    if (message.channel.id !== '1226176448612663376') return;
+
+    
+    if (!message.content.startsWith("py-run")) {
+        
+        message.delete().catch(console.error);
+        return;
+    }
+
+    
+    if (message.content.startsWith("py-run")) {
+        let code = message.content.split("```")[1].split("```")[0];
+        fs.writeFile("file.py", code, (err) => {
+            if (err) {
+                console.log(`error: ${err}`);
+            } else {
+                exec(`python file.py`, (error, stdout, stderr) => {
+                    if (error) {
+                        console.log(error);
+                        return;
+                    } else if (stderr) {
+                        message.reply(stderr);
+                        return;
+                    }
+                    message.reply(stdout);
+                });
+            }
+        });
+    }
+});
+
+client.login(token);
